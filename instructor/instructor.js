10ab80a5fc282503bb1ed8a37e502b7bc383a12c
travis skyles <travis.skyles@gmail.com>
2020-01-29 20:47:57 -0800

    fixed teacher join to be emit from connect; all working!

--- instructor/instructor.js
+++ instructor/instructor.js
@@ -4,7 +4,7 @@ const io = require('socket.io-client');
 // connect to school channel
 const schoolChannel = io.connect('http://localhost:3000/school');
 // join teacher room
-schoolChannel.connect('join', 'teacher');
+schoolChannel.emit('join', 'teacher');
 // grade assigments that are recieved
 schoolChannel.on('submission', payload => {
   let grade = Math.floor(Math.random() * 10);
